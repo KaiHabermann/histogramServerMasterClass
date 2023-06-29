2b83514a3e9bd64727b98e185a93415e081bd47b
Kai Habermann <kai.habermann@gmx.net>
2023-06-29 08:46:00 +0200

    new main file

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 pip install -r app/requirements.txt
-curl -o data/MasterClassAllCuts.root https://cernbox.cern.ch/remote.php/dav/public-files/7ox9EhTvcDo4Tm0/MasterClassAllCuts.root
+curl -o data/MasterClassAllCuts.root https://cernbox.cern.ch/remote.php/dav/public-files/PnsSEfMMdMLxr5P/MasterClassAllCuts.root
